fc8d9fcbbc73687fb275584de3b49a9337354ab2
tkMerc <98034810+GhostDragonWasTaken@users.noreply.github.com>
2023-08-28 11:15:09 +0200

    Launch1

---
+++ version03/chessapp.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'chessapp.exe' with Python DLL being found.
+set PATH=c:\users\marqu\appdata\local\programs\python\PYTHON~2;%PATH%
+set PYTHONHOME=C:\Users\marqu\AppData\Local\Programs\Python\Python311
+"%~dp0.\chessapp.exe" %*
