aa9da4f4afdbc1f607b144d5bd8c22ed9b97abb5
MarquisBurg <98034810+GhostDragonWasTaken@users.noreply.github.com>
2023-09-05 14:48:38 +0200

    Delete version03 directory

--- version03/chessapp.cmd
+++
@@ -1,6 +0,0 @@
-
-@echo off
-rem This script was created by Nuitka to execute 'chessapp.exe' with Python DLL being found.
-set PATH=c:\users\marqu\appdata\local\programs\python\PYTHON~2;%PATH%
-set PYTHONHOME=C:\Users\marqu\AppData\Local\Programs\Python\Python311
-"%~dp0.\chessapp.exe" %*
